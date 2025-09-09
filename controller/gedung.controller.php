71f2b915fe40b16ecb4102b9900054cbabd5c61f
Muhamad Haekal <muhamadhaekal618@gmail.com>
2025-09-09 07:39:02 +0700

    update

--- controller/gedung.controller.php
+++ controller/gedung.controller.php
@@ -7,5 +7,15 @@ class gedungController extends GedungModel
     {
         return $this->findAll();
     }
+    public function getProductBy()
+    {
+        if (!isset($_GET['id'])) {
+            return;
+        }
+        $onlyId = preg_replace('/\D/', '', $_GET['id']);
+        if ($onlyId !== "") {
+            return $this->findOne($_GET['id']);
+        }
+    }
 }
 ?>
